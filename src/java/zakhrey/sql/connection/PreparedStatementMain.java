3adfe35b2aa29f103f6d7b3d8695f16253ebc560
zakhrey <zakhrey@gmail.com>
2025-11-09 13:22:14 +0300

    added statement main classes

---
+++ src/java/zakhrey/sql/connection/PreparedStatementMain.java
@@ -0,0 +1,39 @@
+package zakhrey.sql.connection;
+
+import java.sql.*;
+
+public class PreparedStatementMain {
+
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "root";
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+
+
+    public static void main(String[] args) {
+
+        String preparedQuery = "select * from simple_main.book where release_date > ?::timestamp;";
+        String query = "select * from pg_prepared_statements;";
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement(preparedQuery);
+            Statement statement = connection.createStatement()) {
+            preparedStatement.setString(1, "1997-11-15");
+
+            preparedStatement.executeQuery();
+            preparedStatement.executeQuery();
+            preparedStatement.executeQuery();
+            preparedStatement.executeQuery();
+            preparedStatement.executeQuery();
+
+
+            try(ResultSet resultSet = statement.executeQuery(query)) {
+                while (resultSet.next()) {
+                    System.out.println("name: " + resultSet.getString("name") + " ".repeat(20 - resultSet.getString("name").length()) + "statement: " + resultSet.getString("statement"));
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/java/zakhrey/sql/connection/SimpleConnectionMain.java
@@ -0,0 +1,26 @@
+package zakhrey.sql.connection;
+
+import java.sql.*;
+
+public class SimpleConnectionMain {
+
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "root";
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+                Statement statement = connection.createStatement()) {
+
+            String sql = "select * from simple_main.book where release_date > '1997-11-15';";
+            try(ResultSet resultSet = statement.executeQuery(sql)) {
+                while (resultSet.next()) {
+                    System.out.println("name: " + resultSet.getString("name") + " ".repeat(31 - resultSet.getString("name").length()) + "release date: " + resultSet.getString("release_date"));
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
